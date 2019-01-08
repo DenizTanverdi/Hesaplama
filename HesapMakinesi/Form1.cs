ec554e07852e53ace751792e7b08a4ca5d02cc0a
semvs <semvs@ayvansaray.edu.tr>
2019-01-08 09:41:06 +0300

    Add project files.

---
+++ HesapMakinesi/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace HesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Hesap hesap = new Hesap();
+            MethodInfo[] methods = hesap.GetMethodInfo();
+
+            foreach (var item in methods)
+            {
+                comboBox1.Items.Add(item);
+                comboBox1.DisplayMember = "ıtems.Name";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal sayi1, sayi2;
+            try
+            {
+
+                sayi1 = Convert.ToDecimal(textBox1.Text);
+                sayi2 = Convert.ToDecimal(textBox2.Text);
+
+                Hesap hesap = new Hesap();
+
+
+                MethodInfo methodInfo = (MethodInfo)comboBox1.SelectedItem;
+                decimal sonuc = Convert.ToDecimal(methodInfo.Invoke(hesap,new object[2] { sayi1, sayi2 }));
+
+                label1.Text = sonuc.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ Hesaplama/Hesap.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HesapMakinesi
+{
+    public class Hesap
+    {
+      public decimal Toplama(decimal sayi1,decimal sayi2)
+        {
+            return (sayi1 + sayi2);
+        }
+
+        public decimal Cikarma(decimal sayi1, decimal sayi2)
+        {
+            return (sayi1 - sayi2);
+        }
+
+        public decimal Carpma(decimal sayi1, decimal sayi2)
+        {
+            return (sayi1 * sayi2);
+        }
+
+        public decimal Bolme(decimal sayi1, decimal sayi2)
+        {
+            return (sayi1 / sayi2);
+        }
+
+        public double Karekok(double sayi)
+        {
+            return Math.Sqrt(sayi);
+        }
+
+        public MethodInfo[] GetMethodInfo()
+        {
+            MethodInfo[] methods = typeof(Hesap).GetMethods();
+            return methods;
+        }
+    }
+    
+}
